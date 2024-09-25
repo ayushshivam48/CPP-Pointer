8b01b084f1acad7a0637e240b0eabf7869fea0c9
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-09-25 08:51:21 +0530

    Add files via upload

---
+++ arrayandpointer-1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void process(int *arr , int n){
+    for (int i=0;i<n;i++){
+        cout<<*(arr+i)<<endl;
+    }
+    *(arr+1)=4;
+}
+int main(){
+    int arr[3]={5,1,2};
+    process(arr,3);
+    
+    for (int i=0;i<3;i++){
+        cout<<arr[i]<<" ";
+    }
+}
